8aa74fbfd72cda708674ed852f8c49efa0c7d424
Kafil Nasdami <kafil.nasdami@gmail.com>
2017-02-26 23:32:34 +0000

    Write acceptance test

---
+++ BankKata.Tests/PrintAllStatements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+
+namespace BankKata.Tests
+{
+
+    [TestFixture]
+    public class PrintAllStatements
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [Test]
+        public void PrintAllStatements_MakeSomeTransactions_PrintedSummary()
+        {
+            _account.Deposit(1000);
+            _account.Withdraw(100);
+            _account.Deposit(500);
+
+            var transactions = _repo.AllTransactions();
+
+            _account.PrintStatements();
+
+            _statementPrinter.Verify(x => x.Print(transactions));
+        }
+    }
+
+
+}
