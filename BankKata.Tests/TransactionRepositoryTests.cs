1ec721ddf78ffbf4b1586dd0785ed422ef6cb6f6
Kafil Nasdami <kafil.nasdami@gmail.com>
2017-02-27 02:17:13 +0000

    All TransactionRepository tests passed

--- BankKata.Tests/TransactionRepositoryTests.cs
+++ BankKata.Tests/TransactionRepositoryTests.cs
@@ -28,6 +28,25 @@ namespace BankKata.Tests
             _clock.Verify(x => x.DayOfTransactionFormated());
             Assert.AreEqual(1000,_repo.AllTransactions().First().Amount);
         }
+
+        [Test]
+        public void Repository_Can_Withdraw()
+        {
+            _repo.Withdraw(100);
+            _clock.Verify(x => x.DayOfTransactionFormated());
+            Assert.AreEqual(-100, _repo.AllTransactions().First().Amount);
+        }
+
+        [Test]
+        public void Repository_Can_Return_All_Transactions()
+        {
+            _repo.Deposit(100);
+            _repo.Withdraw(100);
+            _repo.Deposit(200);
+
+            _clock.Verify(x=>x.DayOfTransactionFormated(),Times.Exactly(3));
+            Assert.AreEqual(3,_repo.AllTransactions().Count);
+        }
     }
 
 
