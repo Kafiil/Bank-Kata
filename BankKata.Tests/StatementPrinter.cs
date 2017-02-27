f28488f529afdfb492abf735dd2cd01bbf59cdda
Kafil Nasdami <kafil.nasdami@gmail.com>
2017-02-27 01:36:38 +0000

    StatementPrinter Refactored

--- BankKata.Tests/StatementPrinter.cs
+++ BankKata.Tests/StatementPrinter.cs
@@ -16,19 +16,38 @@ namespace BankKata.Tests
 
         public void Print(List<Transaction> transactions)
         {
-            
             PrintHeader();
 
+            PrintLines(transactions);
+        }
+
+        private void PrintLines(List<Transaction> transactions)
+        {
             int balance = 0;
+            var lines = new List<string>();
 
             foreach (var transaction in transactions)
             {
                 balance += transaction.Amount;
-                _writer.Write(string.Format("{0} | {1} | {2}",
-                    transaction.Date,
-                    transaction.Amount.ToString("F"),
-                    balance.ToString("F"), new CultureInfo("fr-FR")));
+                lines.Add(GetTransactionLine(transaction, balance));
+                
             }
+
+            lines.Reverse();
+
+            foreach (var line in lines)
+            {
+                _writer.Write(line);
+            }
+        }
+
+        private string  GetTransactionLine(Transaction transaction, int balance)
+        {
+            return string.Format("{0} | {1} | {2}",
+                transaction.Date,
+                transaction.Amount.ToString("F"),
+                balance.ToString("F"), new CultureInfo("fr-FR"));
+            
         }
 
         private void PrintHeader()
